c1bdaab052552893dd12d34fdd55f11ae10a2ae3
YANNICK LOIC <yannick.nyami@appsolute.fr>
2022-01-27 13:14:54 +0100

    added retrofit and models

---
+++ app/src/main/java/com/example/birthdaysapp/data/models/ApiResponse.kt
@@ -0,0 +1,6 @@
+package com.example.birthdaysapp.data.models
+
+data class ApiResponse(
+    val dob: Dob,
+    val name: Name
+)
---
+++ app/src/main/java/com/example/birthdaysapp/data/models/Dob.kt
@@ -0,0 +1,6 @@
+package com.example.birthdaysapp.data.models
+
+data class Dob(
+    val age: Int,
+    val date: String
+)
